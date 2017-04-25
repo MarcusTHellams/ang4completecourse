e48e9dfaa1cf741347180b46db09a3e0355a9f9b
CACI\marcus.hellam <marcus.hellam.caci.com>
2017-04-25 13:06:51 -0400

    Creating and outputting the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from './../shared/ingredient.model';
 @Component({
   selector: 'shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Array<any> = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
   constructor() { }
 
   ngOnInit() {
