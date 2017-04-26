0c219ee5acf845645ccef7ec6c0f520ffff2163e
Marcus T. Hellams <mhellams@hotmail.com>
2017-04-26 00:08:16 -0400

    Managing recipes in a recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+    new Recipe('Another test recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  ];
 
   constructor() { }
+
+  public getRecipes: Function = () => {
+    return this.recipes.slice();
+  }
 
 }
