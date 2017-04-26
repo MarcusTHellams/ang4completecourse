2e0e0f40d3b2ba4ec5df06dacbde02b2cbda04af
Marcus T. Hellams <mhellams@hotmail.com>
2017-04-26 01:19:49 -0400

    Add ingredients to recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,10 +1,14 @@
+import { Ingredient } from './../shared/ingredient.model';
+
 export class Recipe {
   public name: String;
   public description: String;
   public imagePath: String;
-  constructor(name: string, desciption: string, imagePath: string) {
+  public ingredients: Ingredient[];
+  constructor(name: string, desciption: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desciption;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,21 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from './../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
   public recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('Another test recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    new Recipe('Tasty Schnitzel', 'A super tasty schnitzel - just awesome!', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger', 'What else do you need to say?', 'http://i.huffpost.com/gen/1034181/images/o-FATBURGER-INTERNATIONAL-LOCATIONS-facebook.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 4),
+      ])
   ];
 
   constructor() { }
